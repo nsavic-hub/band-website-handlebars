17f68a094250037da19ebc1d4bce6b994e139399
Hub <nsavic.hub@gmail.com>
2018-07-18 00:47:12 +1000

    Resized text/images

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 // Start the app!
 const app = require('./app')
-app.set('port', 7777)
+app.set('port', 9000)
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running at’ PORT ${server.address().port}`)
 })
